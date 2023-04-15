910798b41af10dac7b802f46439798bf55a6d869
PdeKingSley <KingWDReal@gmail.com>
2023-04-15 19:27:03 +0800

    init

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
